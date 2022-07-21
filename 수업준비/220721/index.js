f17917efb48f02707f4f4142bc3f2c16c517b3c2
kyuriiii <kyuri99@naver.com>
2022-07-21 16:47:39 +0900

    study: Set nodejs ejs lesson

--- 수업준비/220721/index.js
+++ 수업준비/220721/index.js
@@ -3,7 +3,6 @@ const app = express();              // app을 express 프레임워크로 킨다
 const port = 8080;
 
 app.set("view engine", "ejs");      // app에 view engine을 설치, ejs를 템플릿으로
-app.use(express.static(__dirname + 'views')); // views 폴더 경로는 프로젝트 폴더.(__dirname이 폴더 위치)
 
 app.use( express.static( 'public' ) );
 
