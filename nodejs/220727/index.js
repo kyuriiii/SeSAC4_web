04dff8dc1d3f56142358160f4de11cfc4cdf923e
kyuriiii <kyuri99@naver.com>
2022-07-27 18:50:06 +0900

    study: Axios form send

--- nodejs/220727/index.js
+++ nodejs/220727/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
 const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
@@ -8,26 +8,28 @@ app.use( express.static( "public" ) );
 app.use(express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
 
-app.get("/", function(req,res){
+app.get("/", function(req,res) {
     res.render("index");
-});
-
+})
+app.get("/test", function(req,res){
+    res.send("test");
+})
+app.get("/test2", function(req,res){
+    res.send({name: "test2", message: 123});
+})
 app.get("/receive", function(req,res){
-    console.log( "receive-get" );
     console.log( req.query );
-    res.send(req.query.name + " 안녕!");
-});
-
+    let name = req.query.name;
+    let msg = req.query.name + "GET 안녕";
+    res.send({name: name, message: msg});
+})
 app.post("/receive", function(req,res){
-    console.log( "receive-post" );
     console.log( req.body );
-    res.send(req.body.name + " 안녕!");
-});
-app.post("/receive_form", function(req,res){
-    console.log( "receive-post" );
-    console.log( req.body );
-    res.send(req.body.name + " 안녕! 너는 " + req.body.gender + "구나.");
-});
+    let name = req.body.name;
+    let msg = req.body.name + " 안녕";
+    // res.send();
+    res.send( { name: name, message: msg } );
+})
 
 app.listen(port, ()=>{
     console.log( "Server Port : ", port );
