0515a8cc75f0efc4b0a4a7fe81ccbd199dfdb141
kyuriiii <kyuri99@naver.com>
2022-07-28 18:40:02 +0900

    prepare: Prepare 07/28 file upload lesson

--- 수업준비/220728/index.js
+++ 수업준비/220728/index.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const multer = require("multer");
 
+app.set("view engine", "ejs");
+app.use( express.static( "uploads" ) );
+app.use(express.urlencoded({extended: true}));
+app.use( bodyParser.json() );
+
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, done){
@@ -12,20 +17,22 @@ const upload = multer({
         },
         filename(req, file, done) {
             const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+            done(null, req.body.id + ext);
         },
     }),
     limits: { fileSize: 5*1024*1024 },
 });
 
-app.set("view engine", "ejs");
-app.use( express.static( "public" ) );
-app.use(express.urlencoded({extended: true}));
-app.use( bodyParser.json() );
-
 app.get("/", function(req,res){
     res.render("index");
 });
+app.get("/register", function(req,res) {
+    res.render("register");
+});
+app.post("/register", upload.single('profile'), function(req,res){
+    res.render("result", {img: req.file.filename});
+});
+
 app.post("/upload", upload.single('userfile'), function(req, res){
     console.log( req.file );
     console.log( req.body );
@@ -34,12 +41,12 @@ app.post("/upload", upload.single('userfile'), function(req, res){
 app.post("/upload/array", upload.array('userfile'), function(req, res){
     console.log( req.files );
     console.log( req.body );
-    res.send("Upload");
+    res.send("Upload Array");
 });
 app.post("/upload/fields", upload.fields([{name: 'image1'}, {name:'image2'}]), function(req, res){
     console.log( req.files );
     console.log( req.body );
-    res.send("Upload");
+    res.send("Upload Fields");
 });
 
 app.listen(port, ()=>{
