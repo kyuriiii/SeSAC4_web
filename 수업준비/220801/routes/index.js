2077422474cbd9b46caa3a4e02fa67d76ae40312
kyuriiii <kyuri99@naver.com>
2022-08-02 16:29:19 +0900

    prepare: Prepare 220802 lesson

---
+++ 수업준비/220801/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const user = require("../controller/UserController");
+const router = express.Router();
+
+router.get("/", function(req,res){
+    res.send("Index 페이지입니다.");
+});
+
+router.get("/register", user.get_register);
+router.post("/register", user.post_register);
+
+router.get("/login", user.get_login);
+router.post("/login", user.post_login);
+
+module.exports = router;
