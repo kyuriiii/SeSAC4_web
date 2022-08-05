e17e835d87c68fb1e01bf6c345172cbbca8c2081
kyuriiii <kyuri99@naver.com>
2022-08-05 13:27:02 +0900

    study: Review 220803 practice

--- nodejs/220803/controller/UserController.js
+++ nodejs/220803/controller/UserController.js
@@ -16,16 +16,37 @@ exports.login = (req,res) => {
 }
 exports.post_login = async (req,res) => {
     var data = await User.get_user();
+    "1//1//1//1//\n2//2//2//2//\n"
 
-    var info = data.split("//");
-    // { "asdf", "asdf", "dfdf", "{age}"}
-
-    if ( info[0] != req.body.id ){
-        res.send( "아이디 다름" );
-    } else if ( info[1] != req.body.pw ) {
-        res.send( "비밀번호가 다름" );
-    } else {
-        res.send("로그인 성공!");
+    var infos = data.split("\n");
+    // for ( let i = 0; i < infos.length; i++ ){
+    //     // i = 0, infos[i] = "1//1//1//1";
+    //     var info = infos[i].split("//");
+    //     if ( info[0] == req.body.id && info[1] == req.body.pw ){
+    //         res.send("성공");
+    //         return false;
+    //     }
+    // }
+    var flag = false;
+    for ( let i = 0; i < infos.length; i++ ){
+        // i = 0, infos[i] = "1//1//1//1";
+        var info = infos[i].split("//");
+        if ( info[0] == req.body.id && info[1] == req.body.pw ) flag  = true;
     }
-    console.log( data );
+    
+    if ( flag ) res.send("성공");
+    else res.send("실패");
+    // infos = [ "1//1//1//1", "2//2//2//2" ];
+
+    // var info = data.split("//");
+    // // { "asdf", "asdf", "dfdf", "{age}"}
+
+    // if ( info[0] != req.body.id ){
+    //     res.send( "아이디 다름" );
+    // } else if ( info[1] != req.body.pw ) {
+    //     res.send( "비밀번호가 다름" );
+    // } else {
+    //     res.send("로그인 성공!");
+    // }
+    // console.log( data );
 }
--- nodejs/220803/model/User.js
+++ nodejs/220803/model/User.js
@@ -2,7 +2,7 @@ const fs = require("fs").promises;
 
 exports.post_user = function( data ){
     const { id, pw, name, age } = data;
-    fs.writeFile("./info.txt", `${id}//${pw}//${name}//${age}`);
+    fs.appendFile("./info.txt", `${id}//${pw}//${name}//${age}\n`);
 }
 
 exports.get_user = async function(){
