c607d8628935229bc581a82678bb1473d21cd570
kyuriiii <kyuri99@naver.com>
2022-10-19 11:58:10 +0900

    prepare: How to solve axios cors error

--- 수업준비/react/221019-build-axios/src/App.js
+++ 수업준비/react/221019-build-axios/src/App.js
@@ -6,8 +6,10 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   const getUser = async () => {
+    console.log( process.env.REACT_APP_MODE );
     // let response = await axios.get('http://localhost:8000/react');
-    let response = await axios.get('/react'); // proxy 설정하면
+    let response = await axios.get(process.env.REACT_APP_DB_HOST + '/react'); // proxy 설정하면
+    console.log( response );
     console.log( response.data );
     setUsers(response.data.result );
     setLoading(false);
