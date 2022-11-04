0a2ecff3ff81d12a549d343773809a901039f775
kyuriiii <kyuri99@naver.com>
2022-11-04 12:07:40 +0900

    feat: Initialize spring init project

---
+++ 수업준비/spring/sesac-spring/src/main/java/sesac/sesacspring/SesacSpringApplication.java
@@ -0,0 +1,13 @@
+package sesac.sesacspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesacSpringApplication.class, args);
+	}
+
+}
