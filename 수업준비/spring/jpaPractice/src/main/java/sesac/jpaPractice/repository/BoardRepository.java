260afd71d01f372caba75249fd2fdea8ba86c0ae
kyuriiii <kyuri99@naver.com>
2023-03-18 12:45:48 +0900

    feat: Do jpa practice lesson

--- 수업준비/spring/jpaPractice/src/main/java/sesac/jpaPractice/repository/BoardRepository.java
+++ 수업준비/spring/jpaPractice/src/main/java/sesac/jpaPractice/repository/BoardRepository.java
@@ -1,10 +1,16 @@
 package sesac.jpaPractice.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sesac.jpaPractice.domain.Board;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+	List<Board> findByTitle(String title);
+
+	void deleteByWriter(String writer);
+	// delete from board where writer = #{writer}
 }
